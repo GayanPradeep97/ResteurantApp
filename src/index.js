823b7f4e1cbe83d220b44233ff665549c1be8af5
GayanPradeep97 <gayanpradeep0212@gmail.com>
2022-12-14 23:35:44 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
+
 import "./index.css"; 
+
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from './App'
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(
+    <Router>   
+    <App />
+    </Router>,
+document.getElementById("root")
+);
